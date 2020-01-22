c500de775787b0912a2061ec01eb03947aa773a1
Edwin Sanchez <edwinalex035@gmail.com>
2020-01-21 18:56:04 -0600

    Adding client discovery

--- src/main/java/com/de/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/de/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.de.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients("com.de.microservices.currencyconversionservice.proxy")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
