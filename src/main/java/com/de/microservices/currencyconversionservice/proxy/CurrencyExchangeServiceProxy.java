1b0f15de14030099682bfde9e2cb72169ce6292c
Edwin Sanchez <edwinalex035@gmail.com>
2020-01-23 09:51:05 -0600

    Adding zuul routing to call microservice

--- src/main/java/com/de/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/de/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RibbonClient(name = "currency-exchange-service")
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
